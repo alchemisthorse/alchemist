6095507f91bc20fd147fb8295a546e8fe8a45998
alchemisthorse <fallen.can155@gmail.com>
2026-04-22 14:18:32 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
+# 1. ADD REPOS (CachyOS Kernel & Addons)
+# We use curl to drop the repo files directly into the system
+curl -Lo /etc/yum.repos.d/bieszczaders-kernel-cachyos.repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-40/bieszczaders-kernel-cachyos-fedora-40.repo
+curl -Lo /etc/yum.repos.d/bieszczaders-kernel-cachyos-addons.repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-40/bieszczaders-kernel-cachyos-addons-fedora-40.repo
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# 2. STRIP BLOAT (Handheld & Power stuff)
+# We remove tuned because cachyos-settings and ananicy-cpp handle things better.
+rpm-ostree override remove \
+    waydroid \
+    tuned \
+    tuned-ppd \
+    power-profiles-daemon \
+    steamdeck-dsp \
+    steamdeck-firmware \
+    jupiter-hw-support \
+    jupiter-fan-control
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# 3. THE "SWAP": Replace Fedora Kernel with CachyOS + Install your Addons
+# This single command replaces the heart of the OS.
+rpm-ostree override replace \
+    --from repo=copr:copr.fedorainfraconfig.org:bieszczaders:kernel-cachyos \
+    kernel kernel-core kernel-modules kernel-modules-extra kernel-modules-core \
+    --install kernel-cachyos-devel-matched \
+    --install cachyos-settings \
+    --install cachyos-addons \
+    --install ananicy-cpp \
+    --install cachyos-ananicy-rules \
+    --install mangohud \
+    --install scx-manager \
+    --install scx-scheds
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# 4. ENABLE SERVICES
+# This ensures your tuning starts automatically on boot.
+systemctl enable ananicy-cpp
