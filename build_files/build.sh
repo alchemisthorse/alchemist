e59dd27af21790621fa4c56be053544d219c94d8
alchemisthorse <fallen.can155@gmail.com>
2026-04-22 19:17:17 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,18 +6,22 @@ dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 
 # 2. THE KERNEL SWAP
-# Using the specific LTO package names
+# It is safer to override all three core kernel components at once 
+# to prevent version mismatches during the build.
 rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto \
-    kernel-cachyos-lto
+    kernel-cachyos-lto \
+    kernel-cachyos-lto-core \
+    kernel-cachyos-lto-modules
 
 # 3. FIX FOR DRACUT ERROR: Manual depmod
-# We extract the version string to make it dynamic
-KERNEL_VERSION=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-cachyos-lto)
+# Use 'kernel-cachyos-lto-core' for the version check as it's the most reliable provider
+KERNEL_VERSION=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-cachyos-lto-core)
 depmod -a "$KERNEL_VERSION"
 
 # 4. INSTALL ADDONS & SCHEDULERS
+# We do this in a separate step to ensure the kernel swap is recognized
 rpm-ostree install \
     kernel-cachyos-lto-devel-matched \
     cachyos-settings \
@@ -29,5 +33,9 @@ rpm-ostree install \
     scx-tools
 
 # 5. ENABLE SERVICES
-systemctl enable ananicy-cpp
-systemctl enable scx-loader
+# These will now be baked into the image
+systemctl enable ananicy-cpp.service
+systemctl enable scx-loader.service
+
+# 6. CLEANUP
+dnf5 clean all
